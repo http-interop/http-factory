bd3d29c9ce62f554254c52c22fc54e4d2aadd8bb
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-08-22 10:09:22 +0200

    Added comment about file path

--- src/StreamFactoryInterface.php
+++ src/StreamFactoryInterface.php
@@ -11,6 +11,9 @@ interface StreamFactoryInterface
      *
      * If the argument is a resource, it MUST be readable and SHOULD be seekable. It MAY be writable.
      * If the argument is a string, a temporary resource will be created that is writable and seekable.
+     * 
+     * If the arugment is a string it will be interpided as the content of the stream. File names or 
+     * file paths are not supported. 
      *
      * @param string|resource $resource
      *
